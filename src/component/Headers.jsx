2f49f21ec5261bd2f88dfb65919bd11e2cfada9c
nahuelDev19 <git config --global user.name Your>
2025-01-08 21:05:00 -0300

    cambios en las imagenes

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -19,7 +19,7 @@ export const Headers = ({ cart,removeFromCart,increaseQuantity,decrementQuantity
             <div
               className="carrito"
               >
-              <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+              <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
               <div id="carrito" className="bg-white p-3">
                 {isEmpty ? 
