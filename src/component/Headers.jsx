472f41793cf233e42f2d69a726947987864bb162
nahuelDev19 <git config --global user.name Your>
2025-01-09 14:41:29 -0300

    cambio de direccion de imagen

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -12,7 +12,7 @@ export const Headers = ({ cart,removeFromCart,increaseQuantity,decrementQuantity
         <div className="row justify-content-center justify-content-md-between">
           <div className="col-8 col-md-3">
             <a href="index.html">
-              <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+              <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
             </a>
           </div>
           <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
